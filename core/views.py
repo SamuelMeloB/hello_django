7234ff501423d723866f311bb61e302bf1a78699
Samuel <samuel.barros@ee.ufcg.edu.br>
2019-08-13 02:20:13 -0300

    Commit da atividade de soma na url aplicação Django.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request,nome,idade):
     return HttpResponse('<h1>Hello {} de {} anos. </h1>' .format(nome, idade))
+
+def soma(request,num1,num2):
+    resultado = num1 + num2
+    return HttpResponse('<h2>Aqui está a soma entre {} e {}: {}. </h2>' .format(num1, num2, resultado))
+
+def subtracao(request,num1,num2):
+    resultado = num1 - num2
+    return HttpResponse('<h2>Aqui está a subtração entre {} e {}: {}. </h2>' .format(num1, num2, resultado))
+
+def multiplicacao(request,num1,num2):
+    resultado = num1 * num2
+    return HttpResponse('<h2>Aqui está a multiplicação entre {} e {}: {}. </h2>' .format(num1, num2, resultado))
+
+def divisao(request,num1,num2):
+    resultado = num1 / num2
+    return HttpResponse('<h2>Aqui está a divisão entre {} e {}: {}. </h2>' .format(num1, num2, resultado))
